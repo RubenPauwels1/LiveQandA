4484d028e3633e35ed39ca5bbed14e682fcc5e99
Ruben Pauwels <rubenp.pauwels@gmail.com>
2016-05-14 16:58:39 +0200

    Added listen for new discussions

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 $( document ).ready(function() {
     var socket = io();
-    //NEW DISC
     
+    //NEW DISCUSSION
+    //TALK TO SERVER
     $('#submitDiscussion').click(function(e){
         var newDiscussion = $('#discussionName').val();
         socket.emit("New Discussion", newDiscussion);
         console.log(newDiscussion);
         $('#discussionName').val("");
         return false;
+    })
+    
+    //TALK TO CLIENT
+    socket.on('newDiscussionInDB', function(newDiscussionInDB){
+        var newDiscussion = "<h2 class='discussion'>" + newDiscussionInDB.discussion + "</h2>";
+        $('.discussions').append(newDiscussion);
     })
 });
